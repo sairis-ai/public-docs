221a6c4283f46122ba56c96bf4f3eb2747d0452a
Robert Smith <smith@playerlync.com>
2024-05-10 06:54:53 -0600

    base

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "public-docs",
   description: "NuxAi Plublic Docs",
-  base: '/docs/',
+  base: '/repo/',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
